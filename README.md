477356d1af2ba0e81961ccaab57beb4156abf2c7
Beata Lin <nahoyabe@gmail.com>
2013-09-30 02:46:33 +0800

    Added main script and updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 jquery-youtube
 ==============
 
-A jQuery plugin that inserts a youtube iframe into html elements by the youtube id option.
+A jQuery plugin that inserts a iframe from youtube video url.
+
+```html
+<div class="js-youtube" data-url="http://www.youtube.com/watch?v=DDs5bXh4erM"></div>
+```
+
+## Options
+
+* `url` (Required)  
+    The youtube url of the video.
+
+* `width` (Optional)  
+    Default: 300  
+    Width of the iframe
+
+* `height` (Optional)  
+    Default: 300 * 0.75  
+    Height of the iframe
---
+++ jquery-youtube.js
@@ -0,0 +1,26 @@
+(function($){
+  $.fn.youtube = function () {
+    var updateYoutubeId = function () {
+      var $self = $(this),
+          match = $self.data('url').match(/\/\/[^.]+.youtube(?:-nocookie)?.com\/.*\?.*\bv=([^&]+)/),
+          youtubeId = (match && match[1] ? match[1] : null),
+          width = $self.data('width') || 300,
+          height = $self.data('height') || parseInt(width * 0.75, 10),
+          selfWidth = $self.width();
+
+      if (width > selfWidth) {
+        width = selfWidth;
+        height = parseInt(width * 0.75, 10);
+      }
+
+      $self.
+        data('youtubeId', youtubeId).empty().
+        append('<iframe width="' + width + '" height="' + height + '" src="//www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>');
+    };
+
+    return $(this).on('reload', updateYoutubeId).trigger('reload');
+  };
+
+  $('div.js-youtube').youtube();
+
+}(jQuery));
